b2b54cdc868b99b596e980e6e3378f383128e5ba
Aayush Mishra <mishraayu185@gmail.com>
2026-03-14 21:13:08 +0530

    deploying backend again

--- my-backend/src/main.ts
+++ my-backend/src/main.ts
@@ -9,14 +9,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "https://authentication-and-authorization-dy78bb2g0.vercel.app"
-  ],
-  credentials: true
-}));
-  
+const corsOptions = {
+  origin: 'https://authentication-and-authorization-nx.vercel.app', // exact Vercel URL
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
